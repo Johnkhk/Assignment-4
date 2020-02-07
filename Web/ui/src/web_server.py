d2220ced58380dcda5d07d7efc9b3b07d9764508
Jesus <Jesus@DESKTOP-47N70JQ>
2020-02-07 14:03:07 -0800

    With admin table

--- Web/ui/src/web_server.py
+++ Web/ui/src/web_server.py
@@ -39,7 +39,7 @@ def valid_user(req):
 # Route to validate login credentials...
 def post_login(req):
   if valid_user(req):
-    return render_to_response('templates/portal.html', {}, request = req)
+    return admin(req)
   else:
     return render_to_response('templates/did_log_in.html', {'tag': 'Incorrect Password'}, request = req)
 
@@ -72,11 +72,16 @@ if __name__ == '__main__':
   # Adds a route v2 so you can have localhost:5000/ or localhost:5000/v2
   config.add_route('v2', '/')
   # Loading stuff from the server
-  config.add_view(admin, route_name='v2')
+  config.add_view(portal, route_name='v2')
   
 
   config.add_route('show_users', '/show_users')
-  config.add_view(show_users, route_name='show_users')
+  config.add_view(show_users, route_name='show_users')  
+  
+  config.add_route('admin', '/admin')
+  config.add_view(admin, route_name='admin')
+
+
 
 
   config.add_route('new_user', '/new_user')
